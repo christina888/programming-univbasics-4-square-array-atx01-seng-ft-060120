e152a95b0f966c63e11828fc99df504aff685028
Christina Chianelli <cechianelli@gmail.com>
2020-05-22 04:08:14 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   counter = 0
   new_arr = []
   while array.length |index| do
-    new_arr.push(index)
+    new_arr.push(index*index)
     counter += 1
   end
     return new_arr
